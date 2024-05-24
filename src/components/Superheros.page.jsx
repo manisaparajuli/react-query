0fc96e54802b0c474c010f51e12b4298a602f636
manisaparajuli <manisaparajuli66@gmail.com>
2024-05-24 18:53:16 +1000

    data fetched using axios

--- src/components/Superheros.page.jsx
+++ src/components/Superheros.page.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 export const SuperherosPage = () => {
+
+  const [data, setData] = useState([])
+  const [ loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheroes')
+    .then(res => {
+      setData(res.data)
+      
+      setLoading(false)
+    })
+  }, [])
+
+  if (loading){
+    return <h2>Loading.....</h2>
+  }
+
   return (
-    <div>SuperherosPage.page</div>
+    <div>{
+      data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>
+        
+      })  
+    }</div>
   )
 }
