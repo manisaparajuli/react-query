f408c32a95d71c5a476cf7ac45d128f2e9458be5
manisaparajuli <manisaparajuli66@gmail.com>
2024-05-26 20:59:46 +1000

    data fetching on button click

--- src/components/RQSuperHeros.page.jsx
+++ src/components/RQSuperHeros.page.jsx
@@ -6,17 +6,25 @@ const dataFetching = () => {
 }
 
 export const RQSuperHerosPage = () => {
-  const{pending, data, isError, error, isFetching} = useQuery('super-hero', dataFetching, {cacheTime: 1000,})
+  const{isLoading, data, isError, error, isFetching, refetch} = useQuery('super-hero', dataFetching, 
+  {
+    // cacheTime: 5000,
+    refetchInterval: 2000,
+  })
 
-  if(pending){
+  if(isLoading){
     return <h2>Loading....</h2>
   }
   if (isError){
     return <h2>{error.message}</h2>
   }
+
+  console.log({isLoading, isFetching})
+
   return (
     <div>
       <h2>UseQuery result</h2>
+      <button onClick={refetch}>Fetch Heros</button>
       {
         data?.data.map((hero) => {
           return <p key={hero.id}>{hero.name}</p>
